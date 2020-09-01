1ec26694efeb081a5d3c56bea5ec74a2987d7e75
jun <enskawk1991@gmail.com>
2020-09-01 20:11:17 +0900

    apollo client set

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,1 +1,18 @@
-export default () => "Home";
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  return <h1>Home</h1>;
+};
